2bb787a2c4ea7c35e7fa5c3260cf03489175af57
kclaudeeager <71407602+kclaudeeager@users.noreply.github.com>
2021-12-07 09:11:28 +0200

    Volley  is well used

--- app/src/main/java/com/example/lab1/Registraion_Activity.java
+++ app/src/main/java/com/example/lab1/Registraion_Activity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.lab1.SqliteDB_part.operations_student;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registraion_Activity extends AppCompatActivity {
     EditText fname,lname,regno,departement;
@@ -47,6 +57,7 @@ public class Registraion_Activity extends AppCompatActivity {
                    Students.add(finalstudent_model);
                    finalstudent_model.setStudents(Students);
                    dbHandler.addStudent(finalstudent_model);
+                   SendStudentData(fname.getText().toString(), lname.getText().toString(),regno.getText().toString(), departement.getText().toString());
                    fname.getText().clear();
                    lname.getText().clear();
                    regno.getText().clear();
@@ -76,5 +87,32 @@ public class Registraion_Activity extends AppCompatActivity {
 
         }
         return true;
+    }
+    public void SendStudentData(String fname, String lname, String regno, String departement){
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        String url="http://192.168.43.232//PHP-folder/Registration.php";
+        StringRequest request=new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response:", response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error: ",error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("fname", fname);
+                params.put("lname", lname);
+                params.put("regno",regno);
+                params.put("department", departement);
+                return params;
+            }
+        };
+        requestQueue.add(request);
     }
 }
--- app/src/main/java/com/example/lab1/Student_List.java
+++ app/src/main/java/com/example/lab1/Student_List.java
@@ -12,9 +12,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.lab1.SqliteDB_part.operations_student;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Student_List extends AppCompatActivity {
    static RecyclerView studentRecycler;
@@ -22,7 +34,7 @@ public class Student_List extends AppCompatActivity {
    Student_model student_model;
    StudentAdapter studentAdapter;
     private operations_student dbHandler;
-
+   static ArrayList<Student_model> student_models;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +43,7 @@ public class Student_List extends AppCompatActivity {
         getSupportActionBar().setTitle("List of students");
        // student_model=(Student_model) getIntent().getSerializableExtra("Student");
         dbHandler=new operations_student(this);
-
-        ArrayList<Student_model> student_models=dbHandler.FetchStudents();
+        student_models=dbHandler.FetchStudents();
         Student_model.Students=student_models;
         if(Student_model.getStudents()!=null)
         for(Student_model student_model:Student_model.getStudents()){
@@ -40,14 +51,12 @@ public class Student_List extends AppCompatActivity {
             Students.add(student_model);
         }
 
-        studentRecycler=(RecyclerView) findViewById(R.id.St_list);
-        studentAdapter=new StudentAdapter(Students);
-        studentRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        studentRecycler.setVerticalScrollBarEnabled(true);
-        studentRecycler.setAdapter(studentAdapter);
+        loadList();
     }
 
+
     @Override
+
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -55,6 +64,97 @@ public class Student_List extends AppCompatActivity {
 
         }
         return true;
+    }
+
+    public void loadList(){
+       // contacts=new ArrayList<Contact>();
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        String url="http://192.168.43.232//PHP-folder/List_Students.php";
+        ArrayList<Student_model> students=new ArrayList<>();
+        StringRequest request=new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                          //  Log.d("RESPONSE", response);
+                           // JSONObject jsonObject = null;
+
+                            //jsonObject = new JSONObject(response);
+
+
+                            JSONArray jsonArray=new JSONArray(response);
+                            //Log.d("json:", jsonArray.toString());
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject res = null;
+
+                                res = jsonArray.getJSONObject(i);
+
+                                String fname = null;
+
+                                fname = res.getString("fname");
+
+                                String lname = null;
+
+                                lname = res.getString("lname");
+
+                                String reg = null;
+
+                                reg = res.getString("regno");
+
+                                String dep = null;
+
+                                dep = res.getString("department");
+
+                                Student_model stud = new Student_model(fname, lname, Integer.parseInt(reg), dep);
+
+                                Log.d("remote:", stud.toString());
+                              // if (!student_models.contains(stud)) {
+                                    students.add(stud);
+                                //}
+                            }
+                           for(Student_model student_model1:students) {
+                              // Log.d("st1:", student_model1.toString());
+                               if(!student_models.contains(student_model1))
+                               student_models.add(student_model1);
+                           }
+                            for(Student_model student_model1: student_models) {
+                                Log.d("st2:", student_model1.toString());
+                            }
+                            Student_model.Students=student_models;
+                            if(Student_model.getStudents()!=null)
+                                for(Student_model student_model:Student_model.getStudents()){
+                                    Log.d("Fname: ",student_model.getFname());
+                                    Students.add(student_model);
+                                }
+                            studentRecycler=(RecyclerView) findViewById(R.id.St_list);
+                            studentAdapter=new StudentAdapter(Students);
+                            studentRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                            studentRecycler.setVerticalScrollBarEnabled(true);
+                            studentRecycler.setAdapter(studentAdapter);
+                        }
+                        catch(Exception e){
+
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error",error.getMessage());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+
+        requestQueue.add(request);
+        for(Student_model student_model1:students)
+            Log.d("st1:",student_model1.toString());
+
+
     }
 
 }
